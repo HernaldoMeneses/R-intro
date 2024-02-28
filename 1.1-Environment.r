234f7f2a99bd2dd17f3ff024279b9e37b301396d
HernaldoMeneses <140676491+HernaldoMeneses@users.noreply.github.com>
2024-02-28 14:21:18 -0300

    1.1-environment

---
+++ 1.1-Environment.r
@@ -0,0 +1,7 @@
+#   R is a integrated suite of software facilities for data manipulation, calculation and graphical display.
+##      An effective data handling and storage facility.
+##      A suite of operators for calculations on arrays, in particular matrices.
+##      A large, coherent, integrated collection of intermediate tools for data analysis.
+##      Graphical facilities for data analysis and display either directly at the computer or on hardcopy.
+##      A well developed, simple and effective programming language (called 'S') which includes
+##          conditionals, loops, user defined recursive functions and input an output facilities.
